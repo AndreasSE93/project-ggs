59cfa951f0980ecf9d54ee7e695ec6f0b5e60f3d
Viktor Palerius <Viktor.palerius@gmail.com>
2014-04-23 16:09:51 +0200

    Implemented JSON to client side

---
+++ javaChat/src/Message.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+
+
+public class Message {
+	 String message,username;
+	 String color;
+	Message(){
+		
+	}
+	Message(String message,String username, String color){
+		this.message = message;
+		this.username = username;
+		this.color = color;
+		
+		
+	}
+
+}
