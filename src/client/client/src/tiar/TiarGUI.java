b35ea8667afe376cf44413250170aefa1a3cc0fc
Josef <f4arez@gmail.com>
2014-05-07 13:19:49 +0200

    fixed GUI

--- src/client/client/src/tiar/TiarGUI.java
+++ src/client/client/src/tiar/TiarGUI.java
@@ -3,23 +3,31 @@ package tiar;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Insets;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class TiarGUI {
-	public JPanel a1 = new JPanel();
-	public JPanel a2 = new JPanel();
-	public JPanel a3 = new JPanel();
-	public JPanel b1 = new JPanel();
-	public JPanel b2 = new JPanel();
-	public JPanel b3 = new JPanel();
-	public JPanel c1 = new JPanel();
-	public JPanel c2 = new JPanel();
-	public JPanel c3 = new JPanel();
-	
+	public JButton a1 = new JButton();
+	public JButton a2 = new JButton();
+	public JButton a3 = new JButton();
+	public JButton b1 = new JButton();
+	public JButton b2 = new JButton();
+	public JButton b3 = new JButton();
+	public JButton c1 = new JButton();
+	public JButton c2 = new JButton();
+	public JButton c3 = new JButton();
+	public JButton[] game = new JButton[9];
 	
 	public  void render (ArrayList<String> gameField){
 		
@@ -38,25 +46,87 @@ public class TiarGUI {
 		JPanel middleRow = new JPanel();
 		JPanel bottomRow = new JPanel();
 		
-		topRow.setLayout(new BorderLayout());
-		middleRow.setLayout(new BorderLayout());
-		bottomRow.setLayout(new BorderLayout());
 		
-		topRow.add(this.a1, BorderLayout.LINE_START);
-		topRow.add(this.a2, BorderLayout.AFTER_LAST_LINE);
-		topRow.add(this.a3, BorderLayout.AFTER_LAST_LINE);
+		gamePane.setLayout(new BoxLayout(gamePane, BoxLayout.Y_AXIS));
+		topRow.setLayout(new BoxLayout(topRow, BoxLayout.X_AXIS));
+		middleRow.setLayout(new BoxLayout(middleRow, BoxLayout.X_AXIS));
+		bottomRow.setLayout(new BoxLayout(bottomRow, BoxLayout.X_AXIS));
 		
-		middleRow.add(this.b1, BorderLayout.LINE_START);
-		middleRow.add(this.b2, BorderLayout.AFTER_LAST_LINE);
-		middleRow.add(this.b3, BorderLayout.AFTER_LAST_LINE);
 		
-		bottomRow.add(this.c1, BorderLayout.LINE_START);
-		bottomRow.add(this.c2, BorderLayout.AFTER_LAST_LINE);
-		bottomRow.add(this.c3, BorderLayout.AFTER_LAST_LINE);
+		 JPanel aa1 = new JPanel();
+		 JPanel aa2 = new JPanel();
+		 JPanel aa3 = new JPanel();
+		 JPanel bb1 = new JPanel();
+		 JPanel bb2 = new JPanel();
+		 JPanel bb3 = new JPanel();
+		 JPanel cc1 = new JPanel();
+		 JPanel cc2 = new JPanel();
+		 JPanel cc3 = new JPanel();
 		
-		window.add(topRow);
-		window.add(middleRow);
-		window.add(bottomRow);
+		 aa1.add(a1);
+		 aa2.add(a2);
+		 aa3.add(a3);
+		 bb1.add(b1);
+		 bb2.add(b2);
+		 bb3.add(b3);
+		 cc1.add(c1);
+		 cc2.add(c2);
+		 cc3.add(c3);
+		
+		topRow.add(aa1);
+		topRow.add(aa2);
+		topRow.add(aa3);
+		
+		middleRow.add(bb1);
+		middleRow.add(bb2);
+		middleRow.add(bb3);
+		
+		bottomRow.add(cc1);
+		bottomRow.add(cc2);
+		bottomRow.add(cc3);
+		
+		topRow.setPreferredSize(new Dimension (300, 100));
+		bottomRow.setPreferredSize(new Dimension (300, 100));
+		middleRow.setPreferredSize(new Dimension (300, 100));
+		
+		
+		
+		a1.setPreferredSize(new Dimension(100,100));
+		a2.setPreferredSize(new Dimension(100,100));
+		a3.setPreferredSize(new Dimension(100,100));
+		b1.setPreferredSize(new Dimension(100,100));
+		b2.setPreferredSize(new Dimension(100,100));
+		b3.setPreferredSize(new Dimension(100,100));
+		c1.setPreferredSize(new Dimension(100,100));
+		c2.setPreferredSize(new Dimension(100,100));
+		c3.setPreferredSize(new Dimension(100,100));
+		
+		game[0] = a1;
+		game[1] = a2;
+		game[2] = a3;
+		game[3] = b1;
+		game[4] = b2;
+		game[5] = b3;
+		game[6] = c1;
+		game[7] = c2;
+		game[8] = c3;
+		
+		this.c1.setBackground(Color.BLUE);
+		this.c2.setBackground(Color.RED);
+		this.c3.setBackground(Color.GREEN);
+		
+		this.a1.setBackground(Color.BLUE);
+		this.a2.setBackground(Color.RED);
+		this.a3.setBackground(Color.GREEN);
+		
+		
+		this.b3.setBackground(Color.BLUE);
+		this.b1.setBackground(Color.RED);
+		this.b2.setBackground(Color.GREEN);
+		
+		gamePane.add(topRow);
+		gamePane.add(middleRow);
+		gamePane.add(bottomRow);
 		
 		
 		
@@ -71,8 +141,33 @@ public class TiarGUI {
 	}
 	
 	
+	
+	public void doMove (int position, boolean player) {
+		JButton update = this.game[position];
+		makeJButton(update, player);
+		
+	}
+
+	private void makeJButton (JButton jb,  boolean player){
+		File imageCheck;
+		if (player == true){ imageCheck = new File("resources/cross.png");}
+		else { imageCheck = new File("resources/circle.png");
+		}
+		try {
+			Image img = ImageIO.read(imageCheck);
+			jb.setIcon(new ImageIcon(img));
+		} catch (IOException ex) {
+		}
+		jb.setMargin(new Insets(0, 0, 0, 0));
+		jb.setBorder(null);
+	
+		
+	}
+	
 	public static void main (String args[]){
 		TiarGUI t = new TiarGUI( );
 	t.render(new ArrayList<String>());
+	t.doMove(3, true);
+	t.doMove(4, false);
 	}
 }
