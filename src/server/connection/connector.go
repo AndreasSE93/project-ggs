3b89a8a6e3c47cfb2f75683b53946cbae217a1ef
Andreas Wennerberg <Andreas.Wennerberg.0022@student.uu.se>
2014-05-12 23:35:20 +0200

    Removed unused code

--- src/server/connection/connector.go
+++ src/server/connection/connector.go
@@ -3,7 +3,6 @@ package connection
 import (
 	"net"
 	"bufio"
-	"fmt"
 )
 
 type Connector struct {
@@ -11,34 +10,4 @@ type Connector struct {
 	UserName string
 	Connection net.Conn
 	Scanner *bufio.Scanner
-}
-
-func clientIdGenerator(sender chan int) {
-	id := 0
-	for {
-		id++
-		sender <- id
-	}
-}
-
-func initServerClient(netConn net.Conn, idChan chan int, lobbyContact chan Connector) {
-	client := new(Connector)
-	client.ConnectorID = <- idChan
-	client.Connection = netConn
-	client.CurrentRoom = -1
-	lobbyContact <- *client
-}
-
-func requestListener(idChannel chan int, sendClientChannel chan Connector) {
-	ln, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		fmt.Printf("Error, port listener couldn't be established");
-	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			continue
-		}
-		go initServerClient(conn, idChannel, sendClientChannel)
-	}
 }
