6203ab6d99f4a1950fc279f36663c6a0a9e376a5
Josef <f4arez@gmail.com>
2014-05-13 12:21:16 +0200

    Fixed tictactoe server messages

---
+++ src/client/client/src/packageManaging/TTTMessage.java
@@ -0,0 +1,19 @@
+package packageManaging;
+
+public class TTTMessage {
+	public final int id = 200;
+	 public String user;
+	 public String message;
+	 
+	 public TTTMessage(){
+		 
+		 
+	 }
+
+	public TTTMessage(String message, String user){
+		this.message = message;
+		this.user = user;
+		
+		
+	}
+}
