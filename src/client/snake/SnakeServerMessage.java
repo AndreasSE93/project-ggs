d5b4cdf99a1893f8d32dde6dbf72bda80591a0f0
Josef <f4arez@gmail.com>
2014-05-23 12:58:16 +0200

    added winfunction in achtung

--- src/client/snake/SnakeServerMessage.java
+++ src/client/snake/SnakeServerMessage.java
@@ -5,6 +5,8 @@ public class SnakeServerMessage {
 	final int PacketID = 302;
 	public SnakePlayer[] Players;
 	public boolean clearBoard;
+	public boolean hasWon;
+	public String winnerName;
 
 public SnakeServerMessage(int len){
 	this.Players = new SnakePlayer[len];
